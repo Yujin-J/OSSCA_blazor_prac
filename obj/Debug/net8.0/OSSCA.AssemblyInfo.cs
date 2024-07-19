6b1e48146b728b35c1823755039c64d80baa5591
Yujin Jung <yujin8275@naver.com>
2024-07-19 21:29:17 +0900

    Resolved merge conflicts

--- obj/Debug/net8.0/OSSCA.AssemblyInfo.cs
+++ obj/Debug/net8.0/OSSCA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OSSCA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3306b536a0d1a2f3cce313f43dd2f8634d151d0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OSSCA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OSSCA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
